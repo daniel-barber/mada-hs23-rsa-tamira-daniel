21affb3b43b23f3bd7c3a8515dd60c70482cb493
daniel.barber <daniel.barber@students.fhnw.ch>
2023-10-17 18:40:30 +0200

    create 2 random prime numbers with SecureRandom

--- src/main/java/RSAGenerator.java
+++ src/main/java/RSAGenerator.java
@@ -1,5 +1,20 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class RSAGenerator {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //Random generator
+        SecureRandom random = new SecureRandom();
+
+        //Set bitLength to size analog bitLength of example in sk.txt
+        int bitLength = 1024;
+
+        //Generate 2 prime numbers
+        BigInteger prime1 = BigInteger.probablePrime(bitLength,random);
+        BigInteger prime2 = BigInteger.probablePrime(bitLength,random);
+
+        System.out.println(prime1);
+        System.out.println(prime2);
     }
 }
