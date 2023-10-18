14d3c76404c1f907ca26f827e438e1dd09bff37c
daniel.barber <daniel.barber@students.fhnw.ch>
2023-10-18 22:53:34 +0200

    write test for encrypt method testing small numbers

--- src/test/java/rsagenerator/TextEncryptorTest.java
+++ src/test/java/rsagenerator/TextEncryptorTest.java
@@ -1,4 +1,26 @@
 package rsagenerator;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TextEncryptorTest {
+
+    @Test
+    public void testEncrypt(){
+        //test encryption with values from serie1 A26.1
+
+        //Given
+        BigInteger x = BigInteger.valueOf(9);
+        BigInteger n = BigInteger.valueOf(11);
+        BigInteger e = BigInteger.valueOf(25);
+
+        //When
+        BigInteger result = TextEncryptor.encrypt(x,n,e);
+
+        //Then
+        assertEquals(BigInteger.valueOf(1),result);
+    }
 }
